7dee9c7c79ad0bb0445d9ddb203f234a465470d5
runningW <runningW@noreply.gitcode.com>
2025-09-21 19:14:05 +0800

    修改App.cj

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -7,5 +7,5 @@ export logger_appender_file_level=INFO
 export logger_appender_file_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
 export logger_appender_file_rotateDuration=DAY
 export LD_LIBRARY_PATH=./target/release/common:./target/release/mysql:./target/release/user:$LD_LIBRARY_PATH
-
+export user_version=1.0.0
 fboot run $1 --dylibPattern='controller|common'
