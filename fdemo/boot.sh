6d68e58e21876b1ed728e21383507265021dc33b
runningW <runningW@noreply.gitcode.com>
2025-09-21 14:34:29 +0800

    修改fdemo/boot.sh

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -2,9 +2,9 @@
 
 # pattern可省略，有默认值
 export logger_appender_console_level=ERROR
-export logger_appender_console_pattern=[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m
+export logger_appender_console_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
 export logger_appender_file_level=INFO
-export logger_appender_file_pattern=[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m
+export logger_appender_file_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
 export logger_appender_file_rotateDuration=DAY
 
 fboot run $1 --dylibPattern='controller|common'
