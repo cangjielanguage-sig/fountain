d8807dffdb5905f921a5fde4c651d2680e9c6cf1
runningW <runningW@noreply.gitcode.com>
2025-09-22 21:19:18 +0800

    修改fdemo/boot.sh  App.cj

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-# pattern可省略，有默认值
-export logger_appender_console_level=ERROR
-export logger_appender_console_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-export logger_appender_file_level=INFO
-export logger_appender_file_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-export logger_appender_file_rotateDuration=DAY
-export LD_LIBRARY_PATH=./target/release/boot:./target/release/mysql:./target/release/user:$LD_LIBRARY_PATH
-export version=1.0.0
-fboot run $1 --dylibPattern='bootloader|controller'
+run(){
+    # pattern可省略，有默认值
+    export logger_appender_console_level=ERROR
+    export logger_appender_console_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_file_level=INFO
+    export logger_appender_file_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_file_rotateDuration=DAY
+    export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/mysql:./fdemo/release/user:$LD_LIBRARY_PATH
+    export version=1.0.0
+    fboot run $1 --dylibPattern='bootloader|controller'
+}
+
+build(){
+    export CANGJIE_STDX_PATH=$CANGJIE_STDX_DYNAMIC_PATH
+    cjpm build --target-dir=./fdemo
+}
+
+case "$1" in 
+run)
+    run
+    ;;
+build)
+    build
+    ;;
+esac
+
+exit $?
