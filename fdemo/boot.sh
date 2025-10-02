f483937474bf8b1010788fef818e697fd6371804
runningW <runningW@noreply.gitcode.com>
2025-10-02 14:50:47 +0800

    修改日志，fix StringGenerator

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -5,12 +5,15 @@ echo "target-dir=$path"
 
 run(){
     # pattern可省略，有默认值
-    export logger_console_level=ERROR
-    export logger_console_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-    export logger_file_level=INFO
-    export logger_file_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-    export logger_file_path=./log/fdemo.log
-    export logger_file_rotateDuration=DAY
+    export logger_appender_console=FDemoConsole
+    export logger_appender_FDemoConsole_level=ERROR
+    export logger_appender_FDemoConsole_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_file=FDemoFile
+    export logger_appender_FDemoFile_level=INFO
+    export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_FDemoFile_path=./log/fdemo.log
+    export logger_appender_FDemoFile_rotateDuration=DAY
+    export mvc_port=8888
     export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/opengauss:./fdemo/release/user:$LD_LIBRARY_PATH
     export version=1.0.0
     fboot run $path --dylibPattern='(boot|\.(controller|service\.impl))'
