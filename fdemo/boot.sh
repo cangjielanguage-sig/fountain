24a64043e61e23cb1c9d79623dce8a36d40f7a96
runningW <runningW@noreply.gitcode.com>
2025-10-02 22:19:48 +0800

    修改fdemo配置

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -12,7 +12,7 @@ run(){
     export logger_appender_FDemoFile_level=INFO
     export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
     export logger_appender_FDemoFile_path=./log/fdemo.log
-    export logger_appender_FDemoFile_rotateDuration=MINUTE
+    export logger_appender_FDemoFile_rotateDuration=DAY
     export mvc_port=8080 # 这一行可以没有，默认就是8080
     export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/opengauss:./fdemo/release/user:$LD_LIBRARY_PATH
     fboot run $path --dylibPattern='(boot|\.(controller|service\.impl))'
