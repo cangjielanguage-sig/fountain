3682ae53d4f7a0d02f5ca397ed634e45c805f198
runningW <runningW@noreply.gitcode.com>
2025-10-04 19:08:51 +0800

    增加AuthHandler注释。@RequestHeader增加有参构造函数，用来接收请求头名。MVC注册函数增加debug日志。修改PathPattern。改进boot.sh。

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -3,19 +3,24 @@
 path=$2
 echo "target-dir=$path"
 
-run(){
-    # pattern可省略，有默认值
+exports(){
+    # pattern可省略，有默认值                                                                                                   
     export logger_appender_console=FDemoConsole
-    export logger_appender_FDemoConsole_level=ERROR
-    export logger_appender_FDemoConsole_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-    export logger_appender_file=FDemoFile
+    export logger_appender_FDemoConsole_level=DEBUG
+    export logger_appender_FDemoConsole_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'                                      export logger_appender_file=FDemoFile
     export logger_appender_FDemoFile_level=INFO
-    export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
-    export logger_appender_FDemoFile_path=./log/fdemo.log
+    export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'                                         export logger_appender_FDemoFile_path=./log/fdemo.log
     export logger_appender_FDemoFile_rotateDuration=DAY
-    export mvc_port=8080 # 这一行可以没有，默认就是8080
-    export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/opengauss:./fdemo/release/user:$LD_LIBRARY_PATH
+    export mvc_port=8080 # 这一行可以没有，默认就是8080                                                                         
+    export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/opengauss:./fdemo/release/user:$LD_LIBRARY_PATH     
+}
+run(){
+    exports
     fboot run $path --dylibPattern='(boot|user\.util\.auth|\.(controller|service\.impl))'
+}
+launch(){
+    exports 
+    ./fdemo/release/bin/launcher
 }
 
 build(){
@@ -38,6 +43,9 @@ cleanUpdate)
     ;;
 build)
     build
+    ;;
+launch)
+    launch
     ;;
 esac
 
