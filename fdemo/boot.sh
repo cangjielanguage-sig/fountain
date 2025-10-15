139e7af5fa04b0013561af40df806c9313bf1259
runningW <runningW@noreply.gitcode.com>
2025-10-15 15:49:43 +0800

    修改fdemo/boot.sh增加日志格式说明

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -4,12 +4,18 @@ path=$2
 echo "target-dir=$path"
 
 exports(){
-    # pattern可省略，有默认值                                                                                                   
+    # pattern可省略，有默认值
+    # %level 记录当前日志级别
+    # %name 记录当前日志名称
+    # %d 记录当前日志时间，花括号内是时间格式
+    # %m 记录当前日志消息文本
     export logger_appender_console=FDemoConsole
     export logger_appender_FDemoConsole_level=DEBUG
-    export logger_appender_FDemoConsole_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'                                      export logger_appender_file=FDemoFile
+    export logger_appender_FDemoConsole_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_file=FDemoFile
     export logger_appender_FDemoFile_level=INFO
-    export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'                                         export logger_appender_FDemoFile_path=./log/fdemo.log
+    export logger_appender_FDemoFile_pattern='[%level-%name]%d{yyyy/MM/dd,HH:mm:ss.SSS}|%m'
+    export logger_appender_FDemoFile_path=./log/fdemo.log
     export logger_appender_FDemoFile_rotateDuration=DAY
     export mvc_port=8080 # 这一行可以没有，默认就是8080                                                                         
     export LD_LIBRARY_PATH=./fdemo/release/boot:./fdemo/release/opengauss:./fdemo/release/user:$LD_LIBRARY_PATH     
