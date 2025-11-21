1692944f1ef9244172e2b78479f8029ea2517bc2
runningW <runningW@noreply.gitcode.com>
2025-11-21 10:58:04 +0800

    ``` feat(fdemo): 注释掉数据库连接池相关配置
    将 boot.sh 中与数据库连接池相关的环境变量导出语句注释掉，
    包括连接池大小、检查机制、连接生命周期等配置项。同时注释掉了
    opengauss 的连接 URL 配置，以避免默认启用这些设置。
    ```

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -22,17 +22,17 @@ exports(){
     export mvc_internalServerErrorMessageKind=BEAN
     export mvc_internalServerErrorMessage=NameOf500Handler
 #    export orm_noPool=true # 默认是false，true表示不用连接池
-    export orm_useStdPool=false # 默认是true
-    export orm_drivers=opengauss
-    export orm_databasePoolInitSize=10
-    export orm_databasePoolMinSize=10
-    export orm_databasePoolMaxSize=10
-    export orm_databasePoolCheckOnCreation=true
-    export orm_databasePoolCheckOnBorrowing=true
-    export orm_databasePoolCheckOnReturning=false
-    export orm_databasePoolConnectionLife=86400
-    export orm_databasePoolCheckInterval=300 # 默认是300，单位是秒
-    export orm_databasePoolCheckSql='select 1'
+    # export orm_useStdPool=false # 默认是true
+    # export orm_drivers=opengauss
+    # export orm_databasePoolInitSize=10
+    # export orm_databasePoolMinSize=10
+    # export orm_databasePoolMaxSize=10
+    # export orm_databasePoolCheckOnCreation=true
+    # export orm_databasePoolCheckOnBorrowing=true
+    # export orm_databasePoolCheckOnReturning=false
+    # export orm_databasePoolConnectionLife=86400
+    # export orm_databasePoolCheckInterval=300 # 默认是300，单位是秒
+    # export orm_databasePoolCheckSql='select 1'
 #    export orm_pooledDatasourceMaxSize=1
 #    export orm_pooledDatasourceMaxIdleSize=1
 #    export orm_pooledDatasourceIdleTimeout=86400
@@ -41,7 +41,7 @@ exports(){
 #    export orm_pooledDatasourceKeepaliveTime=86400
     # orm_transactionalFuncExecution 和@Transactional注解只要有一个生效就会将事务切面织入到函数
     export orm_transactionalFuncExecution='*..*.delete*(**): *|*..*.remove*(**): *|*..*.save*(**): *|*..*.add*(**): *|*..*.new*(**): *|*..*.create*(**): *|*..*.insert*(**): *|*..*.update*(**): *|*..*.change*(**): *|*..*.register*(**): *'
-    export opengauss_orm_connectionUrl=$POSTGRES
+#    export opengauss_orm_connectionUrl=$POSTGRES
     if [[ "$path" == "" ]]; then
         path='./fdemo'
     fi
