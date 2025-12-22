ab7e75c5206be1196bb42a9b397e4f0e8e9d5431
runningW <runningW@noreply.gitcode.com>
2025-12-22 11:23:35 +0800

    ``` feat(orm): 添加对第三方连接池的支持
    在 ORMConfig 中新增 useThirdPartyPool 配置项，并在注册数据源时跳过使用第三方连接池的数据源。
    同时更新了 boot.sh 脚本中的注释，说明如何配置以使用第三方连接池。
    ```

--- fdemo/boot.sh
+++ fdemo/boot.sh
@@ -30,7 +30,9 @@ exports(){
     export mvc_internalServerErrorMessageKind=BEAN
     export mvc_internalServerErrorMessage=NameOf500Handler
     # 如果不使用fountain连接池，也不使用标准库连接池，就不要配置以下orm_*Pool*变量，只配置orm_noPool，只能用代码初始化第三方连接池
-#    export orm_noPool=true # 默认是false，true表示不用连接池
+    # export orm_useFountainPool=false # 使用第三方连接池，不使用fountain.orm的连接池，也不使用标准库的连接池。
+    # 此时使用ORM.register(datasource, default: false) # 开发者自己用代码初始化Driver和连接池、调用这个函数注册连接池
+    # export orm_noPool=true # 默认是false，true表示不用连接池
     export orm_useStdPool=false # 默认是true，表示使用标准库连接池，false是使用fountain连接池
     export orm_drivers=opengauss # 逗号分隔的驱动名称
     # orm_databasePool开头的是fountain.orm.DatabasePool的配置项
